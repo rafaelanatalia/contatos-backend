15c00ef4a8cabf1dfa62f36e27b8b37c5eb438a4
Sérgio Moura <smouracalmon@gmail.com>
2021-10-04 19:45:47 -0300

    Registrando usuário - caminho feliz

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,7 +1,16 @@
+const {Usuario} = require('../database/models');
+const bcrypt = require('bcrypt');
+
 module.exports = {
-    registrar: (req, res)=>{
-       console.log('registrando...');
-       return res.send('registrando...');
+    registrar: async (req, res)=>{
+        // As info virão req.body
+        // {"nome":"XXX", "email":"XXX@XXX", "senha":"123456"}
+       
+        let {nome, email, senha} = req.body;
+        let novoUsuario = await Usuario.create({nome, email, senha:bcrypt.hashSync(senha, 10)});
+        delete novoUsuario.senha;
+
+        return res.json(novoUsuario);
     },
     login: (req, res) => {
         console.log('logando...');
