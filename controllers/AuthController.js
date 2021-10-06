fb535ea416d14fcdc9ec6bcb99b5f11c6040811e
Sérgio Moura <smouracalmon@gmail.com>
2021-10-06 19:50:25 -0300

    Login fazendo

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -16,8 +16,29 @@ module.exports = {
         }
 
     },
-    login: (req, res) => {
-        console.log('logando...');
-       return res.send('logando...');
+    login: async (req, res) => {
+        
+        // As info virão no req.body
+        // {"email":"___@_____", "senha":"XXXXXXX"}
+
+        // Capturar as info do body
+        const {email, senha} = req.body;
+
+        // Pesquisar no BD um usuário com o email dado
+        const usuario = await Usuario.findOne({where:{email}});
+
+        // Se não existir usuário, retornar erro.
+        if(!usuario){
+            return res.status(403).json({erro:1, msg:"Acesso negado"});
+        }
+
+        // Existindo o usuário, validar a senha dele (bcrypt)
+        if(!bcrypt.compareSync(senha, usuario.senha)){
+            // Se a senha for inválida, retornar erro.
+            return res.status(403).json({erro:1, msg:"Acesso negado"});
+        }
+
+        // Se a senha for ok, retornar msg sucesso (por enquanto...)
+        res.status(200).json({msg:"We're the champions, my frieends!!!"});
     }
 }
