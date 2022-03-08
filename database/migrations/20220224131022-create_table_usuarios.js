0aac0c2a078f17d395de14771df23a6cb6f6c160
Rafaela <rafaela.natalia1998@hotmail.com>
2022-03-08 10:49:34 -0300

    feita migration de contatos

--- database/migrations/20220224131022-create_table_usuarios.js
+++ database/migrations/20220224131022-create_table_usuarios.js
@@ -2,7 +2,31 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('usuarios', { id: Sequelize.INTEGER });
+    await queryInterface.createTable(
+      'usuarios', 
+      { 
+        id:{
+          type: Sequelize.INTEGER,
+          primaryKey:true,
+          autoIncrement: true,
+          allowNull: false
+      },
+      nome:{
+          type: Sequelize.STRING(100),
+          allowNull: false
+      },
+      email: {
+          type: Sequelize.STRING(100),
+          allowNull: false
+      },
+      senha: {
+          type: Sequelize.STRING(256),
+          allowNull: false
+      }
+      
+    }
+
+      );
   },
 
   async down (queryInterface, Sequelize) {
